3f0fbf6090233385f7ce923964ec3da05cecb0ef
akex <hortinstein@gmail.com>
2023-01-24 22:55:53 -0500

    exported the encryption data structures

--- src/enkodo.nim
+++ src/enkodo.nim
@@ -1,6 +1,20 @@
 import monocypher
 import sysrandom
-import enkodo/types
+
+type
+  Hash* = array[64, byte]
+  Key* = array[32, byte]
+  Nonce* = array[24, byte]
+  Mac* = array[16, byte]
+  Signature* = array[64, byte]
+
+type
+  EncObj* = object
+    publicKey*: Key
+    nonce*:     Nonce
+    mac*:       Mac
+    cipherLen*:  int
+    cipherText*: seq[byte]
 
 #[
   helper function to convert bytes to string
