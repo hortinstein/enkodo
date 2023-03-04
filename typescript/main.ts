39ceeed3e015f07a5b73952d1c5381e260f38188
Alex Hortin <hortinstein@gmail.com>
2023-03-04 02:47:03 -0500

    Update main.ts
    Tes

--- typescript/main.ts
+++ typescript/main.ts
@@ -3,7 +3,7 @@ import * as mod from "https://deno.land/std@0.176.0/crypto/mod.ts";
 // import the nim generated code
 import { createRequire } from "https://deno.land/std@0.176.0/node/module.ts";
 const require = createRequire(import.meta.url);
-const serialize = require("./serialize.js");
+const serialize = require("serialize.js");
 // import {
 //   returnEncObj as returnEncObj_nim,
 //   wrap as wrap_nim,
